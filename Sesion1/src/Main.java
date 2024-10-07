a06cac15bc20630b613c92c0548fbf81ab46f7ca
carmenperezgomez <carmenperezgomez@localhost>
2024-10-07 13:43:15 +0200

    Rama 5 creada

--- Sesion1/src/Main.java
+++ Sesion1/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 			System.out.println("Paso 2: rama 2");
 			System.out.println("Paso 3: rama 3");
 			System.out.println("Paso 4: Version 1 creada");
+			System.out.println("Paso 5: rama 5");
 			
 		}
 }
